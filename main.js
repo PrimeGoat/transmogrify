ce29bc2ebb09e0a6f983c696fc451ddbdb28d32e
DENIS SAVGIR <denis@DENISs-MacBook-Pro.local>
2020-03-08 19:36:48 -0400

    Done

--- main.js
+++ main.js
@@ -1,8 +1,59 @@
+const biggify = function(num) {
+  return num + 9000;
+}
 
+const nasafy = function(num) {
+  out = [];
+  while(num > 0) {
+    out.push(num--);
+  }
+  out.push("Blast off!");
 
+  return out;
+}
 
+const reversify = function(str) {
+  let out = '';
+  for(let i = str.length - 1; i >= 0; i--) {
+    out += str[i];
+  }
+  return out;
+}
 
+const titleify = function(str) {
+  let wasSpace = 1;
+  let out = '';
 
+  for(char of str) {
+    if(char == ' ') {
+      wasSpace = 1;
+    } else {
+      if(wasSpace) {
+        char = char.toUpperCase();
+        wasSpace = 0;
+      } else {
+        char = char.toLowerCase();
+      }
+    }
+    out += char;
+  }
+  return out;
+}
+
+const crazify = function(str) {
+  let upper = false;
+  let out = '';
+
+  for(char of str) {
+    if(upper) {
+      out += char.toUpperCase();
+    } else {
+      out += char.toLowerCase();
+    }
+    upper = (char == ' ') ? upper : !upper;
+  }
+  return out;
+}
 
 if (typeof biggify === 'undefined') {
   biggify = undefined;
